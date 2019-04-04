ca6cdc6c2bfcb34ac6c6e7ca3bc1787a64993775
rosearuge <mariarosaaruge@hotmail.es>
2019-04-04 20:07:08 -0300

    Update index.php

--- index.php
+++ index.php
@@ -10,33 +10,62 @@
     <title>Home</title>
 </head>
 <body>
-   <div class ="container-fluid">
+<div class ="container-fluid">
        <header>
            <?php
            include 'header.php';
            ?>
         </header>
-        <div id="carouselExampleFade" class="carousel slide carousel-fade" data-ride="carousel">
-            <div class="carousel-inner">
-                <div class="carousel-item active carru1">
-      
-                </div>
-                <div class="carousel-item carru2">
-      
-                </div>
-                <div class="carousel-item carru3">
-      
-                </div>
+        <body>
+        <article class= "caja1">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <div class="bd-example">
+    <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+      <ol class="carousel-indicators">
+        <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+        <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+        <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+      </ol>
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img src="img/codigo.jpg" class="d-block w-100" alt="...">
+          <div class="carousel-caption d-none d-md-block">
+            <h5>PROGRAMAR</h5>
+            <p>Full Stack</p>
           </div>
-           <a class="carousel-control-prev" href="#carouselExampleFade" role="button" data-slide="prev">
-             <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-             <span class="sr-only">Previous</span>
-          </a>
-           <a class="carousel-control-next" href="#carouselExampleFade" role="button" data-slide="next">
-              <span class="carousel-control-next-icon" aria-hidden="true"></span>
-              <span class="sr-only">Next</span>
-          </a>
         </div>
+        <div class="carousel-item">
+          <img src="img/fondo3.jpg" class="d-block w-100" alt="...">
+          <div class="carousel-caption d-none d-md-block">
+            <h5>TECNOLOGIA</h5>
+            <p>El futuro es hoy</p>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <img src="img/lugar.jpg" class="d-block w-100" alt="...">
+          <div class="carousel-caption d-none d-md-block">
+            <h5>Clases</h5>
+            <p>Presenciales</p>
+          </div>
+        </div>
+      </div>
+      <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
+    </div>
+  </div>
+</article>
+<footer>
+          <?php
+          include 'footer.php';
+          ?>
+      </footer>
+        </body>
        
 
 
@@ -46,11 +75,7 @@
 
 
 
-      <footer>
-          <?php
-          include 'footer.php';
-          ?>
-      </footer>
+
     
   
 
