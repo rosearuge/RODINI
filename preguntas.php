f84330fcb1e0aea01ad49a309e7f4c574c1b518f
DiegoAlejandro19 <diegoarreglatup@gmail.com>
2019-04-03 23:29:20 -0300

    Actualice las Preguntas Frecuentes

--- preguntas.php
+++ preguntas.php
@@ -7,9 +7,32 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="css/master.css">
 
-    <title>Preguntas</title>
+    <title>Preguntas Frecuentes</title>
 </head>
 <body>
-    
+  <h1>Preguntas Frecuentes</h1>
+  <p>Por medio de la sección de Preguntas Frecuentes, responderemos las dudas más comunes:</p>
+  <ol>
+    <h2><li>¿Qué es Aula Rodini?</li></h2> <!-- A Rodini habría que aplicarle tex transform -->
+    <p>Aula Rodini, es un centro educativo especializado en la formación e-learning (online).<br> Ubicado en Buenos Aires, forma alumnos de todo el país a través de sus campus virtuales.</p>
+    <h2><li>¿Me dan un título al finalizar mi curso?</li></h2>
+    <p>Una vez finalizado y aprobado el curso recibirás un certificado/diploma acreditativo de la formación recibida.<br>El mismo se podrá descargar desde nuestro sitio web, y cuenta con un ID único que le permitirá a cualquier empresa o persona comprobar su veracidad.</p>
+    <h2><li>¿Hay profesores o tutores en el curso?</li></h2>
+    <p>Todos los cursos cuentan con sistema de tutorial de apoyo. Nuestros tutores te ayudarán en todo momento durante el proceso de estudio.</p>
+    <h2><li>¿Qué métodos de pago puedo utilizar? ¿Es seguro el pago?</li></h2>
+    <p>Aceptamos todos los medios de pago de la plataforma <a href="https://www.mercadopago.com.ar/como-pagar">Mercado Pago</a>.<br>El pago es 100% seguro y protegido.</p>
+    <h2><li>¿De cuánto tiempo dispongo para realizar el curso?</li></h2>
+    <p>Cada curso tiene una duración máxima estipulada, esta duración se indica dentro de la información de cada curso.</p>
+    <h2><li>¿Los cursos se pueden comenzar en cualquier momento?</li></h2>
+    <p>Sí. Nuestros cursos están diseñados de tal modo que puedas comenzarlos cuando quieras.</p>
+    <h2><li>¿Hay algún requisito de edad para poder hacer nuestros cursos?</li></h2>
+    <p>Sí. Para inscribirte deberás tener como mínimo 16 años cumplidos.</p>
+    <h2><li>¿Cómo visualizar el contenido del curso?</li></h2>
+    <p>Podrás ver los contenidos de nuestros cursos, apuntes, videos, material interactivo desde la comodidad de tu hogar sin viajes ni traslados.<br>Solo necesitás un dispositivo con conexión a internet.</p>
+    <h2><li>¿Cómo se rinden los exámenes?</li></h2>
+    <p>Se rinden de manera online y con un tiempo determinado.</p>
+    <h2><li>¿Qué pasa si no apruebo un examen?</li></h2>
+    <p>En caso de que no apruebes tendrás la posibilidad de rendir un recuperatorio.</p>
+  </ol>  
 </body>
 </html>
