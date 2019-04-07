d9bfbb7f52430e74245cd35b3773ad45aa2af545
rosearuge <mariarosaaruge@hotmail.es>
2019-04-07 13:22:49 -0300

    Update header.php

--- header.php
+++ header.php
@@ -1,46 +1,45 @@
-
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
-<link href="css/master.css" rel="stylesheet">
-<header class= "cabecera">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+  <link href="css/master.css" rel="stylesheet">
+  <header class="cabecera">
     <ul>
-        <li>
-            <a href="http://twitter.com" target="_blank">
-                <i class="fab fa-2x fa-twitter" style="color:black;"></i>
-            </a>
-        </li>
-        <li>
-            <a href="http://instagram.com" target="_blank">
-                <i class="fab fa-2x fa-instagram" style="color:black;"></i>
-            </a>
-        </li>
-        <li>
-            <a href="http://linkedin.com" target="_blank">
-                <i class="fab fa-2x fa-linkedin-in" style="color:black;"></i>
-            </a>
-        </li>
+      <li>
+        <a href="http://twitter.com" target="_blank">
+          <i class="fab fa-2x fa-twitter" style="color:black;"></i>
+        </a>
+      </li>
+      <li>
+        <a href="http://instagram.com" target="_blank">
+          <i class="fab fa-2x fa-instagram" style="color:black;"></i>
+        </a>
+      </li>
+      <li>
+        <a href="http://linkedin.com" target="_blank">
+          <i class="fab fa-2x fa-linkedin-in" style="color:black;"></i>
+        </a>
+      </li>
 
-  <div id="navbarSupportedContent">
-    <ul>
-      <li>
-        <a class="nav-link" href="index.php">HOME <span class="sr-only">(current)</span></a>
-      </li>
-      <li>
-        <a class="nav-link" href="login.php" >LOGIN</a>
-      </li>
-      <li>
-        <a class="nav-link" href="preguntas.php" >FAQ</a>
-      </li>
-      <li class="compra">
-                <i class="fas fa-2x fa-shopping-cart" style= "color: black"></i>
-        </li>
-    </ul>
-    <br>
-    <br>
-    <form class="fas fa-search" action="" method="" id="search">
-      <input class="form-control mr-sm-2" type="search" placeholder="" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit"></button>
-    </form>
-  </div>
+      <div id="navbarSupportedContent">
+        <ul>
+          <li>
+            <a class="nav-link" href="index.php">HOME <span class="sr-only">(current)</span></a>
+          </li>
+          <li>
+            <a class="nav-link" href="login.php">LOGIN</a>
+          </li>
+          <li>
+            <a class="nav-link" href="preguntas.php">FAQ</a>
+          </li>
+          <li class="compra">
+            <i class="fas fa-2x fa-shopping-cart" style="color: black"></i>
+          </li>
+        </ul>
+        <br>
+        <br>
+        <form class="fas fa-search" action="" method="" id="search">
+          <input class="form-control mr-sm-2" type="search" placeholder="" aria-label="Search">
+          <button class="btn btn-outline-success my-2 my-sm-0" type="submit"></button>
+        </form>
+      </div>
 </nav>
 </header>
