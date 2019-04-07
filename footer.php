b7aa25fc999d388b0606dd2b40b4e214eab54222
rosearuge <mariarosaaruge@hotmail.es>
2019-04-07 13:22:44 -0300

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,41 +1,41 @@
 <footer>
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
-    <div class="row-sm-12">
-        <div class="card-group">
-  <div class="card">
-<a href="<iframe src="https://www.google.com/maps/embed?pb=" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>"    
-<div class="card-body">
-      <h5 class="card-title">Donde estamos?</h5>
-      <p class="card-text">En Av. Monroe 860, Belgrano. Acercate y contanos tu propuesta!</p>
+  <div class="row-sm-12">
+    <div class="card-group">
+      <div class="card">
+        <iframe src=" https://www.google.com/maps/embed?pb=" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+          <div class="card-body">
+            <h5 class="card-title">Donde estamos?</h5>
+            <p class="card-text">En Av. Monroe 860, Belgrano. Acercate y contanos tu propuesta!</p>
+          </div>
+          <div class="card-footer">
+            <small class="text-muted">copy rights</small>
+          </div>
+        </a>
+      </div>
+      <div class="card">
+        <img src="img/noticias.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">NOVEDADES</h5>
+          <p class="card-text">En el mundo de la tecnologia!</p>
+        </div>
+        <div class="card-footer">
+          <small class="text-muted"></small>
+        </div>
+      </div>
+      <div class="card">
+        <img src="img/contactanos.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Contactanos!</h5>
+          <p class="card-text">Telefono: 11-11111111<br>
+            Correo electronico: rodani@gmail.com</p>
+        </div>
+        <div class="card-footer">
+          <small class="text-muted"></small>
+        </div>
+      </div>
     </div>
-    <div class="card-footer">
-      <small class="text-muted">copy rights</small>
-    </div>
-    </a>
   </div>
-  <div class="card">
-    <img src="img/noticias.jpg" class="card-img-top" alt="...">
-    <div class="card-body">
-      <h5 class="card-title">NOVEDADES</h5>
-      <p class="card-text">En el mundo de la tecnologia!</p>
-    </div>
-    <div class="card-footer">
-      <small class="text-muted"></small>
-    </div>
-  </div>
-  <div class="card">
-    <img src="img/contactanos.jpg" class="card-img-top" alt="...">
-    <div class="card-body">
-      <h5 class="card-title">Contactanos!</h5>
-      <p class="card-text">Telefono: 11-11111111<br>
-    Correo electronico: rodani@gmail.com</p>
-    </div>
-    <div class="card-footer">
-      <small class="text-muted"></small>
-    </div>
-  </div>
-</div>
-    </div>
 
 </footer>
