bfeb9e502dcc1d8a7107e65b638059804f3400a1
NicoLacram <nico_lacrampette@hotmail.com.ar>
2019-04-08 17:30:50 -0300

    estructura2

--- index.php
+++ index.php
@@ -17,23 +17,6 @@
            ?>
         </header>
         <div id="carouselExampleFade" class="carousel slide carousel-fade" data-ride="carousel">
-            <div class="carousel-inner">
-                <div class="carousel-item active carru1">
-      
-                </div>
-                <div class="carousel-item carru2">
-      
-                </div>
-                <div class="carousel-item carru3">
-      
-                </div>
-          </div>
-           <a class="carousel-control-prev" href="#carouselExampleFade" role="button" data-slide="prev">
-             <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-             <span class="sr-only">Previous</span>
-          </a>
-           <a class="carousel-control-next" href="#carouselExampleFade" role="button" data-slide="next">
-              <span class="carousel-control-next-icon" aria-hidden="true"></span>
               <span class="sr-only">Next</span>
           </a>
         </div>
