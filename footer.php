9cbbff592c4881d447e1c6559299bd590611d948
NicoLacram <nico_lacrampette@hotmail.com.ar>
2019-04-08 17:33:03 -0300

    estructura2

--- footer.php
+++ footer.php
@@ -1,7 +1,25 @@
-<footer>
-    <div class="row-sm-12">
-        <p>footer</p>
-
+<section class="__wrapper-contact">
+    <div class="row">
+        <article class="col-12 text-center col-sm-6 text-sm-left text-md-left col-lg-4">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3286.185760606546!2d-58.445832484771785!3d-34.54885178047378!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcb436f18dc22f%3A0x36090a5d367889c6!2sAv.+Monroe+860%2C+Buenos+Aires!5e0!3m2!1ses!2sar!4v1554728484585!5m2!1ses!2sar" width="390" height="200" frameborder="0" style="border:0" allowfullscreen></iframe>
+            <h4>Ubicacion</h4>
+            <p><strong>Monroe 860</strong>, CABA<br>+54 11 1234 5678</p>
+        </article>
+        <article class="col-12 text-center col-sm-6 text-sm-left text-md-left col-lg-4">
+            <h5 class="card-title">NOVEDADES</h5>
+            <p class="card-text">En el mundo de la tecnologia!</p>
+        </article>
+        <article class="col-12 text-center col-sm-6 text-sm-left text-md-left col-lg-4">
+            <h5 class="card-title">Contactanos!</h5>
+            <p class="card-text">Telefono: 11-11111111<br>
+            Correo electronico: rodini@gmail.com</p>
+        </article>
     </div>
-
-</footer>
+</section>
+<section class="copyright">
+    <div class="row __max-width">
+        <div class="col-12 text-center">
+            <small>© 2019 <strong>RODINI Aulas</strong>. Todos los izquierdos reservados</small>
+        </div>
+    </div>
+</section>
