6901604a7ab31a3c71fbd934a01efd0132584e7b
NicoLacram <nico_lacrampette@hotmail.com.ar>
2019-04-10 11:45:49 -0300

    estructura final

--- footer.php
+++ footer.php
@@ -1,43 +1,52 @@
-<section class="__wrapper-contact">
+<section class="main-footer">
     <div class="row">
-
-        <article class="col-12 text-center col-sm-6 text-sm-left text-md-left col-lg-4">
-            <h5 class="card-title">NOVEDADES</h5>
-            <p class="card-text">En el mundo de la tecnologia!</p>
-        </article>
-        <article class="col-12 text-center col-sm-6 text-sm-left text-md-left col-lg-4">
-            <h5 class="card-title">Seguinos!</h5>
-            <p class="card-text"><br>
+    <article class="col-12 text-center col-sm-12 text-sm-center col-md-4 text-md-center col-lg-4 text-lg-center">
+            <h4>Seguinos!</h4>
+            <p>
             <ul>
-                    <li>
-                        <a href="http://twitter.com" target="_blank">
-                            <i class="fab fa-2x fa-twitter" style="color:white;"></i>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="http://instagram.com" target="_blank">
-                            <i class="fab fa-2x fa-instagram" style="color:white;"></i>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="http://linkedin.com" target="_blank">
-                            <i class="fab fa-2x fa-linkedin-in" style="color:white;"></i>
-                        </a>
-                    </li>
-                </ul>
+                <li>
+                    <a href="http://twitter.com" target="_blank">
+                      <i class="fab fa-2x fa-twitter" style="color:white;"></i>
+                    </a>
+                </li>
+                <li>
+                    <a href="http://instagram.com" target="_blank">
+                      <i class="fab fa-2x fa-instagram" style="color:white;"></i>
+                    </a>
+                </li>
+                <li>
+                    <a href="http://linkedin.com" target="_blank">
+                      <i class="fab fa-2x fa-linkedin-in" style="color:white;"></i>
+                    </a>
+                </li>
+                <li>
+                    <a href="http://facebook.com" target="_blank">
+                      <i class="fab fa-2x fa-facebook" style="color:white;"></i>
+                    </a>
+                </li>
+                <li>
+                    <a href="http://youtube.com" target="_blank">
+                      <i class="fab fa-2x fa-youtube" style="color:white;"></i>
+                    </a>
+                </li>
+              </ul>
             </p>
         </article>
-        <article class="col-12 text-center col-sm-6 text-sm-left text-md-left col-lg-4">
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3286.185760606546!2d-58.445832484771785!3d-34.54885178047378!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcb436f18dc22f%3A0x36090a5d367889c6!2sAv.+Monroe+860%2C+Buenos+Aires!5e0!3m2!1ses!2sar!4v1554728484585!5m2!1ses!2sar" width="390" height="200" frameborder="0" style="border:0" allowfullscreen></iframe>
-            <h4>Ubicacion</h4>
-            <p><strong>Monroe 860</strong>, CABA<br>+54 11 1234 5678</p>
-        </article>
+      <article class="col-12 text-center col-sm-6 text-sm-center col-md-4 text-md-center col-lg-4 text-lg-center">
+        <h4>Teléfono:</h4>
+        <p><strong>+54 11 1234-5678</strong></p>
+        <h4>E-mail:</h4>
+        <p><strong>consultas@aulasrodini.com.ar</strong></p>
+      </article>
+      <article class="col-sm-12 text-center col-md-4 text-center col-lg-4 text-center">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3286.1859350918708!2d-58.4458324850543!3d-34.54884736210327!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcb436f18dc22f%3A0x36090a5d367889c6!2sAv.+Monroe+860%2C+Buenos+Aires!5e0!3m2!1ses!2sar!4v1554854570997!5m2!1ses!2sar" width="350" height="200" frameborder="0" style="border:0" allowfullscreen></iframe>
+      </article>
     </div>
 </section>
 <section class="copyright">
-    <div class="row __max-width">
+    <div class="row">
         <div class="col-12 text-center">
-            <small>© 2019 <strong>RODINI Aulas</strong>. Todos los izquierdos reservados</small>
+            <small>© 2019 <strong>Aulas RoDiNi</strong>. Todos los derechos reservados</small>
         </div>
     </div>
 </section>
