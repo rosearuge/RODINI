7cca59107a0ba79cc176f3cdb8d166b54c62e9a1
DiegoAlejandro19 <diegoarreglatup@gmail.com>
2019-04-29 01:18:42 -0300

    Modificaciones responsive

--- login.php
+++ login.php
@@ -1,7 +1,7 @@
 <?php
 include_once("controladores/funciones.php");
 if($_POST){
-  
+
   $errores= validar($_POST,"login");
   if(count($errores)==0){
     $usuario = buscarEmail($_POST["email"]);
@@ -20,7 +20,7 @@ if($_POST){
           exit;
         }
       }
-    } 
+    }
   }
 }
 ?>
@@ -55,7 +55,7 @@ if($_POST){
         </ul>
     <?php endif;?>
     <form action="" method="POST">
-      <h2>Inicio de Sesion</h2> 
+      <h2>Inicio de Sesión</h2> 
       <input name='email' placeholder='Email' type='text' value="<?=isset($errores["email"])? "":inputUsuario("email") ;?>"/><hr>
       <input id='password' name='password' placeholder='Contraseña' type='password' value=""/><hr>
       <div class='recordarte'>
@@ -63,21 +63,21 @@ if($_POST){
           <label for='remember'></label>Recuerdame
       </div>
       <input type='submit' value='Iniciar Sesion'/>
-      <a class='forgot' href='#'>Olvidaste tu contraseña?</a>
+      <a class='forgot' href='#'>¿Olvidaste tu contraseña?</a>
     </form>
   </div>
   <section class="caja2">
-    
+
     </section>
-</div> 
+</div>
 <footer>
     <?php
       include 'footer.php';
     ?>
-  </footer> 
+  </footer>
 
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>   
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </body>
 </html>
