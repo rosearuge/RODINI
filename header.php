7ebfaf5e44103dff9f0dca940d07443478c5d6d4
rosearuge <mariarosaaruge@hotmail.es>
2019-04-29 19:25:11 -0300

    Update header.php

--- header.php
+++ header.php
@@ -1,10 +1,39 @@
-<nav class="navbar fixed-top"  style="background-color: #CFCFB1">
-  <a class="navbar-brand" href="#">
-    <img src="img/rodini-logo.jpg" width="65" height="50" alt="logo">
-  </a>
-  <a href="index.php"><button type="button" class="btn btn-default navbar-btn">HOME</button></a>
-  <a href="preguntas.php"><button type="button" class="btn btn-default navbar-btn">FAQ</button></a>
-  <a href="login.php"><button type="button" class="btn btn-default navbar-btn">LOG IN</button></a>
-  <a href="formulario.php"><button type="button" class="btn btn-default navbar-btn">REGISTRARSE</button></a>
-</nav>
-</header>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <title> Mi Cabecera Fija </title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="css/master.css">
+  <script src="https://code.jquery.com/jquery-3.4.0.min.js"></script>
+  <title>Document</title>
+</head>
+<body>
+
+<header class="header">
+<nav class="navbar fixed-top">
+    <a href="#" class="logo">RODINI</a>
+    <span class="menu-icon"> <img src="https://img.icons8.com/color/48/000000/menu.png"> </span>
+  <nav class="navigation">
+    <ul>
+      <li>
+        <a href="index.php">HOME</a>
+      </li>
+      <li>
+        <a href="preguntas.php">FAQ</a>
+      </li>
+      <li>
+        <a href="login.php">LOGIN</a>
+      </li>
+      <li>
+        <a href="formulario.php">REGISTRO</a>
+      </li>
+    </ul>
+  </nav>
+</header>  
+<script src="https://code.jquery.com/jquery-3.4.0.min.js"></script>
+<script src= "java/scripts.js"></script>
+</body>
+</html>
