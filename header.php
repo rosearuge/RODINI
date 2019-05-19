241dc8a3349491edcc48d7e7cdc558c6da5b2d33
NicoLacram <nico_lacrampette@hotmail.com.ar>
2019-05-19 16:08:58 -0300

    codigo atomizado1

--- header.php
+++ header.php
@@ -1,17 +1,3 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-  <title> Mi Cabecera Fija </title>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" href="css/master.css">
-  <script src="https://code.jquery.com/jquery-3.4.0.min.js"></script>
-  <title>Document</title>
-</head>
-<body>
-
 <header class="header">
 <nav class="navbar fixed-top">
     <a href="#" class="logo">RODINI</a>
@@ -33,7 +19,3 @@
     </ul>
   </nav>
 </header>  
-<script src="https://code.jquery.com/jquery-3.4.0.min.js"></script>
-<script src= "java/scripts.js"></script>
-</body>
-</html>
