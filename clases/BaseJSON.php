6a2eef326dbe05ad04fdc2ab6644b2ed7ee8a4c7
DiegoAlejandro19 <diegoarreglatupc@gmail.com>
2019-05-20 21:14:53 -0300

    Update BaseJSON

--- clases/BaseJSON.php
+++ clases/BaseJSON.php
@@ -32,54 +32,39 @@ class BaseJSON extends BaseDatos{
         if(file_exists($this->nombreArchivo)){
             $baseDatosJson= file_get_contents($this->nombreArchivo);
             $baseDatosJson = explode(PHP_EOL,$baseDatosJson);
-            //Aquí saco el ultimo registro, el cual está en blanco
             array_pop($baseDatosJson);
-            //Aquí recooro el array y creo mi array con todos los usuarios 
             foreach ($baseDatosJson as  $usuarios) {
                 $arrayUsuarios[]= json_decode($usuarios,true);
             }
-            //Aquí retorno el array de usuarios con todos sus datos
             return $arrayUsuarios;
         }else{
             return null;
-        }    
+        }
     }
 
     public function jsonRegistroOlvide($email,$password){
         $usuarios = $this->abrirBaseDatos();
-        
+
         foreach ($usuarios as $key=>$usuario) {
-            
+
             if($email==$usuario["email"]){
-                //Esta línea se las comente para que a futuro puedan probar si la clave nueva la van a grabar correctamente, la idea es verla antes de hashearla. le pueden aplicar un dd() y verificar que les trae
-                //$usuario["password"]= $datos["password"];
-                $usuario["password"]= Encriptar::hashPassword($password);
-                //Aquí guardamos el registro del usuario, pero con el password hasheado
-                $usuarios[$key] = $usuario;    
+              $usuario["password"]= Encriptar::hashPassword($password);
+              $usuarios[$key] = $usuario;
             }
-            //Si no es el usuario, entonces va de igual forma a guardar todo los usuarios
-            $usuarios[$key] = $usuario;    
+            $usuarios[$key] = $usuario;
         }
-        
-        //Esto se los coloque para que sepan que con esta función podemos borrar un archivo
+
         unlink($this->nombreArchivo);
-        //Aquí vuelvo a recorrer el array para poder guardar un registro bajo el otro, haciendo uso de la constante de php PHP_EOL
         foreach ($usuarios as  $usuario) {
-            $jsusuario = json_encode($usuario);
-            file_put_contents($this->nombreArchivo,$jsusuario. PHP_EOL,FILE_APPEND);
+          $jsusuario = json_encode($usuario);
+          file_put_contents($this->nombreArchivo,$jsusuario. PHP_EOL,FILE_APPEND);
         }
-    
-     
-    //Esta función no retorna nada, ya que su  responsabilidad es guardar al usuario, pero con su nueva contraseña
     }
-        
+      //Estos métodos se utilizarán más adelante como parte del CRUD
     public function leer(){
-        //A futuro trabajaremos en esto
     }
     public function actualizar(){
-        //A futuro trabajaremos en esto
     }
     public function borrar(){
-        //A futuro trabajaremos en esto
     }
 }
