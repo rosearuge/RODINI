1ca0f975d2e7de4096a8c8da21596da59f6fa79b
NicoLacram <nico_lacrampette@hotmail.com.ar>
2019-05-20 21:47:09 -0300

    final

--- index.php
+++ index.php
@@ -1,14 +1,16 @@
 <!DOCTYPE html>
 <html lang="es">
 <head>
-  <?php include_once ("cabecera.php");
+  <?php
+  include ('cabecera.php');
   ?>
   <title>Home</title>
 </head>
 
 <body>
   <header>
-    <?php include_once ("header.php");
+    <?php
+      include 'header.php';
     ?>
   </header>
   <div class ="container-fluid">
@@ -21,11 +23,11 @@
           <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
         </ol>
         <div class="carousel-inner">
-        <div class="carousel-item active slider1">
-        <div class="carousel-caption d-none d-md-block">
-          <h5>programar</h5>
-          <p>Full Stack</p>
-        </div>
+          <div class="carousel-item active slider1">
+          <div class="carousel-caption d-none d-md-block">
+            <h5>programar</h5>
+            <p>Full Stack</p>
+          </div>
         </div>
         <div class="carousel-item slider2">
         <div class="carousel-caption d-none d-md-block">
@@ -34,6 +36,7 @@
         </div>
         </div>
         <div class="carousel-item slider3">
+
         <div class="carousel-caption d-none d-md-block">
           <h5>clases</h5>
           <p>A distancia</p>
@@ -56,13 +59,13 @@
             <article class="articulo1 col-xs-12 col-md-6 col-lg-6">
               <h2>Full Stack</h2>
                 <img class="imagen" src="img/contactanos.jpg">
-              <p class="descrip">Este programa está diseñado para entrenar en 720 horas repartidas en 4 u 8 meses, a personas sin ninguna experiencia previa en programación y convertirlos en un Full Stack Developer empleable en cualquier proyecto de desarrollo. Si no sabés nada de programación y estás motivado para encarar un desafío intensivo e iniciarte en una nueva profesión, éste es tu programa.</p>
-            </article>
-            <article class="articulo2 col-xs-12 col-md-6 col-lg-6">
-              <h2>Mobile IOS</h2>
-                <img class="imagen" src="img/noticias.jpg">
-              <p class="descrip">Convertite en un programador Mobile iOS certificado, estudiá con los expertos y adquirí las herramientas y bases para transformar tu carrera. Aprendé a crear aplicaciones mobile sólidas, funcionales y atractivas para iOS.</p>
-            </article>
+                    <p class="descrip">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laboriosam sed suscipit dolorem. Ullam, deleniti veritatis. Veniam eius a facilis quod numquam, unde aperiam similique aliquam reprehenderit incidunt impedit quas molestias.</p>
+                </article>
+                <article class="articulo2 col-xs-12 col-md-6 col-lg-6">
+                    <h2>Mobile IOS</h2>
+                        <img class="imagen" src="img/noticias.jpg">
+                    <p class="descrip">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quaerat aut iste recusandae commodi architecto nobis ad, officiis reiciendis saepe minima, optio in neque quod. Veniam earum repellendus cupiditate libero illum?</p>
+                </article>
             <article class="articulo1 col-xs-12 col-md-6 col-lg-6">
               <h2>Diseño gráfico</h2>
                 <img class="imagen" src="img/dis_grafico.jpg">
@@ -74,8 +77,7 @@
               <p class="descrip">Aprende a dominar los principales aspectos y conceptos del Marketing Digital para llevar a cabo estrategias de comunicación eficientes donde aplicarás los diferentes canales que te desarrollarán como un estratega digital profesional.</p>
             </article>
   </section>
-<<<<<<< HEAD
- 
+   
 </div> 
 <footer>
     <?php
@@ -85,13 +87,3 @@
 <?php
 include ('finalpagina.php');
 ?>
-=======
-    <section class="caja2">
-    </section>
-</div>
-
-<footer>
-    <?php include_once ("footer.php"); ?>
-</footer>
-    <?php include_once ("finalpagina.php"); ?>
->>>>>>> 6a2eef326dbe05ad04fdc2ab6644b2ed7ee8a4c7
